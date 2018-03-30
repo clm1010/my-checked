76a5e9022f493059750e928186735eefa7bf22a4
CLM <limin_chu@163.com>
2018-03-30 15:32:37 +0800

    my-checked.js
    my-checked.js

--- js/my-checked.js
+++ js/my-checked.js
@@ -1,18 +1,20 @@
 ;(function($) {
   // 单选框
   $.fn.clradio = function(options) {
-    $(":radio+label", this).each(function(i, v) {
+    var self = this;
+    return $(':radio+label', this).each(function() {
       $(this).addClass('cl-radio');
       if ($(this).prev().is('checked')) {
         $(this).addClass('cl-radio-checked');
       }
     }).click(function(event) {
-      $(this).siblings().removeClass("cl-radio-checked");
+      $(this).siblings().removeClass('cl-radio-checked');
       if (!$(this).prev().is(':checked')) {
-        $(this).addClass("cl-radio-checked");
+        $(this).addClass('cl-radio-checked');
         $(this).prev()[0].checked = true;
       }
       event.stopPropagation();
     }).prev().hide();
   }
+
 })(jQuery)
